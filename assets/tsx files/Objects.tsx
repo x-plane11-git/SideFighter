e8c88dbdc6ef552751ae61b4249e51c49de4ab7a
x-plane11-git <81062925+x-plane11-git@users.noreply.github.com>
2022-05-28 00:05:43 -0400

    Textures and Basemap initialization

---
+++ assets/tsx files/Objects.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="Objects" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+ <image source="../../../../../Downloads/Texture/TX Props.png" trans="000000" width="512" height="512"/>
+</tileset>
